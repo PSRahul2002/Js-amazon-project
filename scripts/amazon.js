af8a04b312558b0ad2b85116fba6c291d888beb8
PSRahul2002 <psrahul2002@gmail.com>
2024-09-07 15:10:17 +0530

    Use products.js file for data of products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,37 +1,7 @@
-const products = [
-    {
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 45,
-        count: 87,
-      },
-      priceCents: 1090,
-    },
-    {
-      image: "images/products/intermediate-composite-basketball.jpg",
-      name: "Intermediate Size Basketball",
-      rating: {
-        stars: 40,
-        count: 127,
-      },
-      priceCents: 2095,
-    },
-    {
-      image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-      name: "Adults Plain Cotton T-Shirt - 2 Pack",
-      rating: {
-        stars: 45,
-        count: 56,
-      },
-      priceCents: 799,
-    },
-  ];
-  
-  let productsHTML = "";
-  
-  products.forEach((product) => {
-    const html = `<div class="product-container">
+let productsHTML = "";
+
+products.forEach((product) => {
+  const html = `<div class="product-container">
             <div class="product-image-container">
               <img class="product-image"
                 src="${product.image}">
@@ -43,7 +13,7 @@ const products = [
   
             <div class="product-rating-container">
               <img class="product-rating-stars"
-                src="images/ratings/rating-${product.rating.stars}.png">
+                src="images/ratings/rating-${(product.rating.stars)*10}.png">
               <div class="product-rating-count link-primary">
                 ${product.rating.count}
               </div>
@@ -79,8 +49,8 @@ const products = [
               Add to Cart
             </button>
           </div>`;
-      
-      productsHTML += html;
-  });
-  
-  document.querySelector('.products-grid').innerHTML = productsHTML;
+
+  productsHTML += html;
+});
+
+document.querySelector(".products-grid").innerHTML = productsHTML;
