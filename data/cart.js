2a8b6a95f726763213c61868f1142e6dd86397ac
PSRahul2002 <psrahul2002@gmail.com>
2024-09-08 19:56:39 +0530

    Fixed radio selectors

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,4 @@
-export const cart = [
-    {
-        productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-        quantity: 1,
-    },
-    {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1,
-    }
-];
+export let cart = JSON.parse(localStorage.getItem('cart')) ||[];
 
 export function addToCart (productId) {
     let matchingItem;
@@ -26,6 +17,8 @@ export function addToCart (productId) {
         quantity: 1,
       });
     }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
   }
 
   export function getCartQuantity (cart){
