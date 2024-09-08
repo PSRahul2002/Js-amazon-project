c69ddb2c34a7a64f468a6118b23be6860ed5fa98
PSRahul2002 <psrahul2002@gmail.com>
2024-09-08 19:38:19 +0530

    Generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [
+    {
+        productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+        quantity: 1,
+    },
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+    }
+];
 
 export function addToCart (productId) {
     let matchingItem;
@@ -11,16 +20,12 @@ export function addToCart (productId) {
   
     if (matchingItem) {
       matchingItem.quantity += 1;
-    //   cartQuantity += 1;
     } else {
       cart.push({
         productId: productId,
         quantity: 1,
       });
-    //   cartQuantity += 1;
     }
-  
-    // document.querySelector(".cart-quantity").innerHTML = cartQuantity;
   }
 
   export function getCartQuantity (cart){
