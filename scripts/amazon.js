6fd55192477d4837ba119c541fbc499155f7d547
PSRahul2002 <86890267+PSRahul2002@users.noreply.github.com>
2024-09-27 15:17:00 +0530

    Update amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart, getCartQuantity} from "../data/cart.js";
+import { cart, addToCart, getCartQuantity} from "../../data/cart.js";
 import { products } from "../data/products.js";
 import { } from "../data/cart-class.js";
 
